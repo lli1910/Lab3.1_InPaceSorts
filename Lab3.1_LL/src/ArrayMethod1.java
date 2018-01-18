0def2ef6bef25ccf188da4b8307d43035a61ec86
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-18 09:22:26 -0500

    lalala

--- Lab3.1_LL/src/ArrayMethod1.java
+++ Lab3.1_LL/src/ArrayMethod1.java
@@ -1,4 +1,27 @@
-
 public class ArrayMethod1 {
+   //bubble sort
+	public static void main(String args[]) {
+		String[] bubble = {"Delta","Alpha", "Charlie", "Beta"};
+		bubbleSort(bubble);
+		System.out.println(bubble);
+	}
+	
+	public static void bubbleSort(String[] stringArr) {
+		for(int i = 0; i < stringArr.length - 2; i++) {
+			//remember swap value
+			//if arr element is greater than the next array element, swap
+		}
+	}
+	
+	public static void intSwap(int[]arr, int index1, int index2) {
+		int num = arr[index1];
+		arr[index1]=arr[index2];
+		arr[index2]=num;
+	}
+	public static void stringSwap(String[]arr, int index1, int index2) {
+		String string = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = string;
+	}
 
 }
