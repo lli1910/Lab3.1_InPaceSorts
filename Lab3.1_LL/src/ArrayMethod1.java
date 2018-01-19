93d0cd443878d467028309293110df05f9ed7f44
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-19 09:22:42 -0500

    yoyoyo

--- Lab3.1_LL/src/ArrayMethod1.java
+++ Lab3.1_LL/src/ArrayMethod1.java
@@ -2,16 +2,38 @@ public class ArrayMethod1 {
    //bubble sort
 	public static void main(String args[]) {
 		String[] bubble = {"Delta","Alpha", "Charlie", "Beta"};
+		int [] insert = {2,3,6,5,4,1};
 		bubbleSort(bubble);
 		System.out.println(bubble);
 	}
 	
+	
+	public static void insertionSort(int [] list1) {
+		//https://www.geeksforgeeks.org/insertion-sort/
+		
+		
+		
+	}
+	
+	public static void selectionSort(double [] list1) {
+		
+		
+		
+	}
+	
 	public static void bubbleSort(String[] stringArr) {
+		int swap = 0;
+		
 		for(int i = 0; i < stringArr.length - 2; i++) {
 			//remember swap value
 			//if arr element is greater than the next array element, swap
+			
 		}
 	}
+	
+	
+
+	
 	
 	public static void intSwap(int[]arr, int index1, int index2) {
 		int num = arr[index1];
