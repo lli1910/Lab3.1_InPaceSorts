fdcfbcd0577045fac790aa56e3f511f3bd895d1f
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-02-27 09:21:28 -0500

    finishedd

--- Lab3.1_LL/src/ArrayMethod1.java
+++ Lab3.1_LL/src/ArrayMethod1.java
@@ -1,9 +1,30 @@
+import java.util.Arrays;
+
 public class ArrayMethod1 {
    //bubble sort
 	public static void main(String args[]) {
-		String[] bubble = {"Delta","Alpha", "Charlie", "Beta"};
-		int [] insert = {4,3,2,5,1};
-		double [] select = {1.2,7.8,7.8,2.3,5.6,3.4};
+		//Testing arrays
+		int[] test1 = {1,4,4,5,2,4,3,17,0};
+		double [] test2 = {1.1, 4.1, 4.2, 5.0, 2.0, 3.0, 4.0, 3.0, 17.0, 0.0};
+		String [] test3 = {"zebra","tortilla", "abba", "foo","bar", "aba"};
+		
+		//Insertion Sort Test
+		long start = System.nanoTime();
+		insertionSort(test1);
+		long end = System.nanoTime();
+		long time = end -start;
+		System.out.println("Test1 took: " + time+ " nanosec");
+		System.out.println(Arrays.toString(test1));
+		
+		//Selection Sort test
+		start = System.nanoTime();
+		
+		
+		
+		
+		String[] bubble = {"Delta","Alpha", "Charlie", "Beta","Delta"};
+		int [] insert = {4,3,2,5,5,1,1};
+		double [] select = {1.2,7.8,7.8,2.3,5.6,7.8,3.4};
 		
 		bubbleSort(bubble);
 		for(String s:bubble) {
@@ -21,31 +42,19 @@ public class ArrayMethod1 {
 		}
 	}
 	
-	//equal//where the swap it
 	public static void insertionSort(int [] list1) {
-		/*
-		for(int i = 0; i<list1.length; i++ ) {
-			for(int j=i+1;j<list1.length;j++) {
-				if(list1[j]<list1[i]) {
-					intSwap(list1, i, j);
-					//i--;
-					j--;
-				}
-				else if(list1[j]>list1[i]) {
-					break;
-				}
-			}	
-		}
-		*/
 		int swapNum = 0;
-		for(int i = 1; i<list1.length;i++) {
+		for(int i = 0; i<list1.length;i++) {
+			swapNum = i;
 			for(int j=i-1; j>=0;j--) {
-				if(list1[i]<list1[j]) {
-					swapNum = j;
+				if(list1[swapNum]<list1[j]) {
+					intSwap(list1, swapNum, j);
+					swapNum--;
+				}
+				else if(list1[swapNum]>list1[j]) {
 					break;
 				}
 			}
-			intSwap(list1, i, swapNum);
 		}
 		
 		
